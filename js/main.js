67b21d45af63cf1a089c953f1b8e08aca7b45574
Goldenfoxs <space6941@gmail.com>
2025-10-04 20:31:43 +0300

    updete

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(function () {
     $('.slider__inner').slick(
         {
             dots: true,
-            arrows: false,
+            arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><img src='image/arrow-left.svg'></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><img src='image/arrow-right.svg'></button>",
         }
     )
 })
