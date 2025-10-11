9bea9e7da0ccb35b9b48921f041e78aba918b2ae
Goldenfoxs <space6941@gmail.com>
2025-10-11 13:04:24 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ $(function () {
             arrows: true,
             prevArrow:"<button type='button' class='slick-prev pull-left'><img src='image/arrow-left.svg'></button>",
             nextArrow:"<button type='button' class='slick-next pull-right'><img src='image/arrow-right.svg'></button>",
+            responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                        arrows: false,
+                        // dots: false,
+                }
+            }
+            ]
         }
     )
 })
